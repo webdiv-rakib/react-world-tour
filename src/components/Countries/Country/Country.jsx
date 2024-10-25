ed7cf71bbb562d8a969e207c8bd4c21ce28cf7b7
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-25 09:36:10 +0600

    dynamic data show

--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,9 +1,11 @@
 import './Country.css'
 const Country = ({ country }) => {
-    const { name,flags } = country;
+    const { name,flags,population,area } = country;
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
+            <h4>Population: {population}</h4>
+            <h4>Area: {area}</h4>
             <img src={flags.png} alt="" />
         </div>
     );
