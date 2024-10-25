a432a3c213e2b40637db9830d592610ca8aacb7e
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-25 10:16:07 +0600

    interactive button added and can useState in any component

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "./Country/Country";
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
@@ -13,9 +14,11 @@ const Countries = () => {
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
-            {
-                countries.map(country => <Country key={country.cca3} country = {country}></Country>)
-            }
+            <div className='country-container'>
+                {
+                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({ country }) => {
-    const { name,flags,population,area } = country;
+    const { name, flags, population, area, cca3 } = country;
+
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
+            <img className='flag' src={flags.png} alt="" />
             <h4>Population: {population}</h4>
             <h4>Area: {area}</h4>
-            <img src={flags.png} alt="" />
+            <p>Code: {cca3}</p>
+            <button onClick={handleVisited}>{visited?'Visited' : 'Not Visited yet'}</button>
+            {visited ? "I have visited this country." : "I want to visit"}
         </div>
     );
 };
